f8efce664659f9559659e35544425fcaf008d94e
Pankaj Rawat <pankajrawat19sept@gmail.com>
2016-12-13 09:21:03 +0530

    Deployment fixeswq

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,8 @@ use Mix.Config
 config :pinterest_backend, PinterestBackend.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  #cache_static_manifest: "priv/static/manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  # cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
