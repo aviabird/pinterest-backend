438e67870c0cd25e1a4b8dfca02d41c47b3a1914
Pankaj Rawat <pankajrawat19sept@gmail.com>
2016-12-13 17:41:48 +0530

    fixes

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,5 +23,4 @@ config :pinterest_backend, PinterestBackend.Endpoint,
 config :pinterest_backend, PinterestBackend.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
